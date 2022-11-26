71fec1563a18e608e554956a763c122223babf08
karinka1901 <karindomagalska@gmail.com>
2022-11-26 22:47:25 +0200

    done

---
+++ Module3/module3-starters-main/module3-starters-main/t9/9.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('input');
+const button = document.querySelector('#start');
+
+button.addEventListener('click', calculate);
+
+function calculate(){
+    console.log(input.value);
+    const num = input.toString().split(' ');
+    var a = parseInt(num[0]);
+    var b = parseInt(num[2]);
+    console.log(a);
+    // if (input.includes('+')){
+    //     num[0]
+    // }
+    // if (input.includes('-')){}
+    // if (input.includes('*')){}
+    // if (input.includes('/')){}
+}
+
